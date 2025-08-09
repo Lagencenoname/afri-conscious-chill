7dc007e045c4f536b1c06d9b84f85a15562783b0
sikllshub23 <digitalskillshub.pro@gmail.com>
2025-08-09 12:54:05 +0100

    Update FacebookPostSection.tsx

--- src/components/FacebookPostSection.tsx
+++ src/components/FacebookPostSection.tsx
@@ -9,19 +9,6 @@ const FacebookPostSection = () => {
           </h2>
           
           <div className="space-y-6 animate-fade-in-up" style={{ animationDelay: '0.2s' }}>
-            <div className="w-full max-w-[500px] mx-auto">
-              <iframe 
-                src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02yx1nvMZK8nTptmW79JBJ9KhDjesmj657Po1E2Hss7monMH86dqEvFGV5ZNXL4a6il%26id%3D61555937974681&show_text=true&width=500" 
-                width="100%" 
-                height="350" 
-                style={{ border: 'none', overflow: 'hidden' }} 
-                scrolling="no" 
-                frameBorder="0" 
-                allowFullScreen={true} 
-                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-                className="w-full h-[380px] sm:h-[420px] md:h-[450px]"
-              />
-            </div>
             
             <div className="w-full max-w-[500px] mx-auto">
               <iframe 
@@ -34,6 +21,20 @@ const FacebookPostSection = () => {
                 allowFullScreen={true} 
                 allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
                 className="w-full h-[520px] sm:h-[580px] md:h-[620px]"
+              />
+            </div>
+            
+            <div className="w-full max-w-[500px] mx-auto">
+              <iframe 
+                src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3Dpfbid02yx1nvMZK8nTptmW79JBJ9KhDjesmj657Po1E2Hss7monMH86dqEvFGV5ZNXL4a6il%26id%3D61555937974681&show_text=true&width=500" 
+                width="100%" 
+                height="350" 
+                style={{ border: 'none', overflow: 'hidden' }} 
+                scrolling="no" 
+                frameBorder="0" 
+                allowFullScreen={true} 
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                className="w-full h-[380px] sm:h-[420px] md:h-[450px]"
               />
             </div>
           </div>
